dbd10b0ff314ddd33e65f38895adbc90a7f421a3
thomappp <143356129+thomappp@users.noreply.github.com>
2023-08-28 09:36:48 +0200

    Add files via upload

--- client.lua
+++ client.lua
@@ -80,6 +80,55 @@ local ExitBinocularsMode = function()
     TriggerServerEvent("simple_v:binoculars_disabled")
 end
 
+local Button = function(controlButton)
+    N_0xe83a3e3557a56640(controlButton)
+end
+
+local RegisterButton = function(id, controls, text)
+    PushScaleformMovieFunction(scaleformButton, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(id)
+
+    for _, control in pairs(controls) do
+        Button(GetControlInstructionalButton(2, control, true))
+    end
+
+    BeginTextCommandScaleformString("STRING")
+    AddTextComponentScaleform(text)
+    EndTextCommandScaleformString()
+    PopScaleformMovieFunctionVoid()
+end
+
+local SetupScaleform = function(scaleformSelected)
+    scaleformButton = RequestScaleformMovie(scaleformSelected)
+    while not HasScaleformMovieLoaded(scaleformButton) do
+        Citizen.Wait(0)
+    end
+
+    DrawScaleformMovieFullscreen(scaleformButton, 255, 255, 255, 0, 0)
+
+    PushScaleformMovieFunction(scaleformButton, "CLEAR_ALL")
+    PopScaleformMovieFunctionVoid()
+    
+    PushScaleformMovieFunction(scaleformButton, "SET_CLEAR_SPACE")
+    PushScaleformMovieFunctionParameterInt(200)
+    PopScaleformMovieFunctionVoid()
+
+    RegisterButton(0, { Config.toggleThermalVision.controlId }, "Vision thermique")
+    RegisterButton(1, { 97, 96 }, "Utiliser le zoom")
+
+    PushScaleformMovieFunction(scaleformButton, "DRAW_INSTRUCTIONAL_BUTTONS")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleformButton, "SET_BACKGROUND_COLOUR")
+    PushScaleformMovieFunctionParameterInt(0)
+    PushScaleformMovieFunctionParameterInt(0)
+    PushScaleformMovieFunctionParameterInt(0)
+    PushScaleformMovieFunctionParameterInt(80)
+    PopScaleformMovieFunctionVoid()
+
+    return scaleformButton
+end
+
 RegisterCommand(Config.command, function()
 
     local currentTime = GetGameTimer()
@@ -180,55 +229,6 @@ Citizen.CreateThread(function()
         end
     end
 end)
-
-local Button = function(controlButton)
-    N_0xe83a3e3557a56640(controlButton)
-end
-
-local RegisterButton = function(id, controls, text)
-    PushScaleformMovieFunction(scaleformButton, "SET_DATA_SLOT")
-    PushScaleformMovieFunctionParameterInt(id)
-
-    for _, control in pairs(controls) do
-        Button(GetControlInstructionalButton(2, control, true))
-    end
-
-    BeginTextCommandScaleformString("STRING")
-    AddTextComponentScaleform(text)
-    EndTextCommandScaleformString()
-    PopScaleformMovieFunctionVoid()
-end
-
-local SetupScaleform = function(scaleformSelected)
-    scaleformButton = RequestScaleformMovie(scaleformSelected)
-    while not HasScaleformMovieLoaded(scaleformButton) do
-        Citizen.Wait(0)
-    end
-
-    DrawScaleformMovieFullscreen(scaleformButton, 255, 255, 255, 0, 0)
-
-    PushScaleformMovieFunction(scaleformButton, "CLEAR_ALL")
-    PopScaleformMovieFunctionVoid()
-    
-    PushScaleformMovieFunction(scaleformButton, "SET_CLEAR_SPACE")
-    PushScaleformMovieFunctionParameterInt(200)
-    PopScaleformMovieFunctionVoid()
-
-    RegisterButton(0, { Config.toggleThermalVision.controlId }, "Vision thermique")
-    RegisterButton(1, { 97, 96 }, "Utiliser le zoom")
-
-    PushScaleformMovieFunction(scaleformButton, "DRAW_INSTRUCTIONAL_BUTTONS")
-    PopScaleformMovieFunctionVoid()
-
-    PushScaleformMovieFunction(scaleformButton, "SET_BACKGROUND_COLOUR")
-    PushScaleformMovieFunctionParameterInt(0)
-    PushScaleformMovieFunctionParameterInt(0)
-    PushScaleformMovieFunctionParameterInt(0)
-    PushScaleformMovieFunctionParameterInt(80)
-    PopScaleformMovieFunctionVoid()
-
-    return scaleformButton
-end
 
 AddEventHandler('onResourceStop', function(resourceName)
     if resourceName == GetCurrentResourceName() then
